e9e8dd937f87c0131eeebe474ad51e15bec0f444
Pavan S <pavanshivanand90@gmail.com>
2024-08-11 14:53:50 +0530

    second commit

--- app.java
+++ app.java
@@ -1,6 +1,5 @@
 class helloworld{
-    public static void main(string[] args) {
-        System.out.println("Hello World");
+    public static <string> void main(string[] args){
+        System.out.println("Hello World Git Hub");
     }
 }
-
